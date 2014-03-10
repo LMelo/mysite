95a83feb95bb515ecd67566a9b7e17c78eb8e3c6
LMelo <luis.nickelback@gmail.com>
2014-03-10 20:03:36 -0300

    Parte 2 - ok

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    """
+    * admin.StackedInline mostra em blocos.
+    * admin.TabularInline mostra em tabelas.
+    """
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    """
+    * fields e fieldsets nao podem estar declarados juntos no modelo.
+    * 'classes': ['collapse'] exibe e esconde o fieldset.
+    * list_display sao as colunas que vao aparecer na listagem do objeto.
+    * list_filter criar uma coluna de filtro a direita.
+    """
+
+    # fields = ['pub_date', 'question']
+
+    fieldsets = [
+        (None, {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Poll, PollAdmin)
